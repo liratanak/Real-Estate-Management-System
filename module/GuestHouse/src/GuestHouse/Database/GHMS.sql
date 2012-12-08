14f2602ca31d930fe5f211ee3a76c38fe36de36f
Liratanak <annliratanak@gmail.com>
2012-12-08 10:15:08 +0700

    Convert ER to Relational

--- module/GuestHouse/src/GuestHouse/Database/GHMS.sql
+++ module/GuestHouse/src/GuestHouse/Database/GHMS.sql
@@ -1,6 +1,7 @@
--- CREATE DATABASE ghms
---   DEFAULT CHARACTER SET utf8
---   DEFAULT COLLATE utf8_general_ci;
+DROP DATABASE ghms;
+CREATE DATABASE ghms
+  DEFAULT CHARACTER SET utf8
+  DEFAULT COLLATE utf8_general_ci;
 
 USE ghms;
 
@@ -18,15 +19,61 @@ CREATE TABLE `users` (
 	`validTimeStart` int(11) UNSIGNED NOT NULL DEFAULT '0',
 	`validTimeEnd` int(11) UNSIGNED NOT NULL DEFAULT '0',
 
-	`firstName` varchar(50) NOT NULL DEFAULT '',
-	`lastName` varchar(50) NOT NULL DEFAULT '',
-	`sex` boolean,
-	`email` varchar(50) NOT NULL DEFAULT '',
-	`lastLoginTime` int(11) UNSIGNED NOT NULL DEFAULT '0',
-	`typeId` int(11) UNSIGNED NOT NULL DEFAULT '0',
 	`userName` varchar(50) NOT NULL DEFAULT '',
 	`passWord` varchar(50) NOT NULL DEFAULT '',
-	`phoneNumber` int(11) UNSIGNED NOT NULL DEFAULT '0',
+	`email` varchar(50) NOT NULL DEFAULT '',
+	`lastLoginTime` int(11) UNSIGNED NOT NULL DEFAULT '0',	
+
+	`groupUid` int(11) UNSIGNED NOT NULL DEFAULT '0',
+
+	PRIMARY KEY (`uid`)
+)ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+DROP TABLE `personal_details`;
+CREATE TABLE `personal_details` (
+	`uid` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+	`pid`  int(11) UNSIGNED NOT NULL DEFAULT '0',
+	`hidden` tinyint(1) UNSIGNED NOT NULL DEFAULT '0',
+	`disabled` tinyint(1) UNSIGNED NOT NULL DEFAULT '0',
+	`deleted` tinyint(1) UNSIGNED NOT NULL DEFAULT '0',
+	`createdTime` int(11) UNSIGNED NOT NULL DEFAULT '0',
+	`createdUserUid` int(11) UNSIGNED NOT NULL DEFAULT '0',
+	`lastModifiedTime` int(11) UNSIGNED NOT NULL DEFAULT '0',
+	`lastModifiedUserUid` int(11) UNSIGNED NOT NULL DEFAULT '0',
+	`validTimeStart` int(11) UNSIGNED NOT NULL DEFAULT '0',
+	`validTimeEnd` int(11) UNSIGNED NOT NULL DEFAULT '0',
+
+	`firstname` varchar(50) NOT NULL DEFAULT '',
+	`lastname` varchar(50) NOT NULL DEFAULT '',
+	`gender` char(1) NOT NULL DEFAULT '',
+	`job` varchar(50) NOT NULL DEFAULT '',
+	`age` tinyint(2) UNSIGNED NOT NULL DEFAULT '0',
+	`email` varchar(50) NOT NULL DEFAULT '',
+	`phoneNumber1` varchar(50) NOT NULL DEFAULT '',
+	`phoneNumber2` varchar(50) NOT NULL DEFAULT '',
+	`others` TEXT NOT NULL ,
+	
+	`userUid` int(11) UNSIGNED NOT NULL DEFAULT '0',
+	`personalTypeUid` int(11) UNSIGNED NOT NULL DEFAULT '0',
+
+	PRIMARY KEY (`uid`)
+)ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+DROP TABLE `personal_types`;
+CREATE TABLE `personal_types` (
+	`uid` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+	`pid`  int(11) UNSIGNED NOT NULL DEFAULT '0',
+	`hidden` tinyint(1) UNSIGNED NOT NULL DEFAULT '0',
+	`disabled` tinyint(1) UNSIGNED NOT NULL DEFAULT '0',
+	`deleted` tinyint(1) UNSIGNED NOT NULL DEFAULT '0',
+	`createdTime` int(11) UNSIGNED NOT NULL DEFAULT '0',
+	`createdUserUid` int(11) UNSIGNED NOT NULL DEFAULT '0',
+	`lastModifiedTime` int(11) UNSIGNED NOT NULL DEFAULT '0',
+	`lastModifiedUserUid` int(11) UNSIGNED NOT NULL DEFAULT '0',
+	`validTimeStart` int(11) UNSIGNED NOT NULL DEFAULT '0',
+	`validTimeEnd` int(11) UNSIGNED NOT NULL DEFAULT '0',
+
+	`title` varchar(50) NOT NULL DEFAULT '',
 
 	PRIMARY KEY (`uid`)
 )ENGINE=InnoDB DEFAULT CHARSET=utf8;
@@ -52,6 +99,64 @@ CREATE TABLE `sizes` (
 	PRIMARY KEY (`uid`)
 )ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+DROP TABLE `groups`;
+CREATE TABLE `groups` (
+	`uid` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+	`pid`  int(11) UNSIGNED NOT NULL DEFAULT '0',
+	`hidden` tinyint(1) UNSIGNED NOT NULL DEFAULT '0',
+	`disabled` tinyint(1) UNSIGNED NOT NULL DEFAULT '0',
+	`deleted` tinyint(1) UNSIGNED NOT NULL DEFAULT '0',
+	`createdTime` int(11) UNSIGNED NOT NULL DEFAULT '0',
+	`createdUserUid` int(11) UNSIGNED NOT NULL DEFAULT '0',
+	`lastModifiedTime` int(11) UNSIGNED NOT NULL DEFAULT '0',
+	`lastModifiedUserUid` int(11) UNSIGNED NOT NULL DEFAULT '0',
+	`validTimeStart` int(11) UNSIGNED NOT NULL DEFAULT '0',
+	`validTimeEnd` int(11) UNSIGNED NOT NULL DEFAULT '0',
+
+	`title` int(11) UNSIGNED NOT NULL DEFAULT '0',
+
+	PRIMARY KEY (`uid`)
+)ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+DROP TABLE `permissions`;
+CREATE TABLE `permissions` (
+	`uid` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+	`pid`  int(11) UNSIGNED NOT NULL DEFAULT '0',
+	`hidden` tinyint(1) UNSIGNED NOT NULL DEFAULT '0',
+	`disabled` tinyint(1) UNSIGNED NOT NULL DEFAULT '0',
+	`deleted` tinyint(1) UNSIGNED NOT NULL DEFAULT '0',
+	`createdTime` int(11) UNSIGNED NOT NULL DEFAULT '0',
+	`createdUserUid` int(11) UNSIGNED NOT NULL DEFAULT '0',
+	`lastModifiedTime` int(11) UNSIGNED NOT NULL DEFAULT '0',
+	`lastModifiedUserUid` int(11) UNSIGNED NOT NULL DEFAULT '0',
+	`validTimeStart` int(11) UNSIGNED NOT NULL DEFAULT '0',
+	`validTimeEnd` int(11) UNSIGNED NOT NULL DEFAULT '0',
+
+	`title` int(11) UNSIGNED NOT NULL DEFAULT '0',
+
+	PRIMARY KEY (`uid`)
+)ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+DROP TABLE `groups_permissions`;
+CREATE TABLE `groups_permissions` (
+	`uid` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+	`pid`  int(11) UNSIGNED NOT NULL DEFAULT '0',
+	`hidden` tinyint(1) UNSIGNED NOT NULL DEFAULT '0',
+	`disabled` tinyint(1) UNSIGNED NOT NULL DEFAULT '0',
+	`deleted` tinyint(1) UNSIGNED NOT NULL DEFAULT '0',
+	`createdTime` int(11) UNSIGNED NOT NULL DEFAULT '0',
+	`createdUserUid` int(11) UNSIGNED NOT NULL DEFAULT '0',
+	`lastModifiedTime` int(11) UNSIGNED NOT NULL DEFAULT '0',
+	`lastModifiedUserUid` int(11) UNSIGNED NOT NULL DEFAULT '0',
+	`validTimeStart` int(11) UNSIGNED NOT NULL DEFAULT '0',
+	`validTimeEnd` int(11) UNSIGNED NOT NULL DEFAULT '0',
+
+	`groupUid` int(11) UNSIGNED NOT NULL DEFAULT '0',
+	`permissionsUid` int(11) UNSIGNED NOT NULL DEFAULT '0',
+
+	PRIMARY KEY (`uid`)
+)ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
 DROP TABLE `rooms`;
 CREATE TABLE `rooms` (
 	`uid` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
@@ -66,11 +171,12 @@ CREATE TABLE `rooms` (
 	`validTimeStart` int(11) UNSIGNED NOT NULL DEFAULT '0',
 	`validTimeEnd` int(11) UNSIGNED NOT NULL DEFAULT '0',
 
-	`toilet` tinyint(1) UNSIGNED NOT NULL DEFAULT '0',
 	`roomNumber` int(11) UNSIGNED NOT NULL DEFAULT '0',
 	`cost` int(11) UNSIGNED NOT NULL DEFAULT '0',
+	`toilet` tinyint(1) UNSIGNED NOT NULL DEFAULT '0',
+	`kitchen` tinyint(1) UNSIGNED NOT NULL DEFAULT '0',
 	`available` tinyint(1) UNSIGNED NOT NULL DEFAULT '0',
-	`kitchen` tinyint(11) UNSIGNED NOT NULL DEFAULT '0',
+	
 	`sizeUid` int(11) UNSIGNED NOT NULL DEFAULT '0',
 
 	PRIMARY KEY (`uid`)
@@ -94,16 +200,18 @@ CREATE TABLE `houses` (
 	`cost` int(11) UNSIGNED NOT NULL DEFAULT '0',
 	`available` tinyint(1) UNSIGNED NOT NULL DEFAULT '0',
 	`otherInfo` TEXT  NOT NULL ,
-	`usersUid` int(11) UNSIGNED NOT NULL DEFAULT '0',
-	`typesUid` int(11) UNSIGNED NOT NULL DEFAULT '0',
+
+	`userUid` int(11) UNSIGNED NOT NULL DEFAULT '0',
+	`typeUid` int(11) UNSIGNED NOT NULL DEFAULT '0',
+	`roomUid` int(11) UNSIGNED NOT NULL DEFAULT '0',
 	`addressUid` int(11) UNSIGNED NOT NULL DEFAULT '0',
 
 
 	PRIMARY KEY (`uid`)
 )ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-DROP TABLE `types`;
-CREATE TABLE `typesHouse` (
+DROP TABLE `house_types`;
+CREATE TABLE `house_types` (
 	`uid` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
 	`pid`  int(11) UNSIGNED NOT NULL DEFAULT '0',
 	`hidden` tinyint(1) UNSIGNED NOT NULL DEFAULT '0',
@@ -116,7 +224,7 @@ CREATE TABLE `typesHouse` (
 	`validTimeStart` int(11) UNSIGNED NOT NULL DEFAULT '0',
 	`validTimeEnd` int(11) UNSIGNED NOT NULL DEFAULT '0',
 
-	`nameType` varchar(50) NOT NULL DEFAULT '',
+	`title` varchar(50) NOT NULL DEFAULT '',
 
 	PRIMARY KEY (`uid`)
 )ENGINE=InnoDB DEFAULT CHARSET=utf8;
@@ -143,53 +251,6 @@ CREATE TABLE `address` (
 	`district` varchar(70) NOT NULL DEFAULT '',
 	`quarter` varchar(70) NOT NULL DEFAULT '',
 	`city` varchar(70) NOT NULL DEFAULT '',
-
-	PRIMARY KEY (`uid`)
-)ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-DROP TABLE `contacts`;
-CREATE TABLE `contacts` (
-	`uid` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
-	`pid`  int(11) UNSIGNED NOT NULL DEFAULT '0',
-	`hidden` tinyint(1) UNSIGNED NOT NULL DEFAULT '0',
-	`disabled` tinyint(1) UNSIGNED NOT NULL DEFAULT '0',
-	`deleted` tinyint(1) UNSIGNED NOT NULL DEFAULT '0',
-	`createdTime` int(11) UNSIGNED NOT NULL DEFAULT '0',
-	`createdUserUid` int(11) UNSIGNED NOT NULL DEFAULT '0',
-	`lastModifiedTime` int(11) UNSIGNED NOT NULL DEFAULT '0',
-	`lastModifiedUserUid` int(11) UNSIGNED NOT NULL DEFAULT '0',
-	`validTimeStart` int(11) UNSIGNED NOT NULL DEFAULT '0',
-	`validTimeEnd` int(11) UNSIGNED NOT NULL DEFAULT '0',
-
-	`phoneNumber` int(11) UNSIGNED NOT NULL DEFAULT '0',
-	`emergencyContacts` int(11) UNSIGNED NOT NULL DEFAULT '0',
-	`emailAddress` varchar(70) NOT NULL DEFAULT '',
-
-	`guestUid` int(11) UNSIGNED NOT NULL DEFAULT '0',
-	`userUid` int(11) UNSIGNED NOT NULL DEFAULT '0',
-
-	PRIMARY KEY (`uid`)
-)ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-DROP TABLE `guests`;
-CREATE TABLE `guests` (
-	`uid` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
-	`pid`  int(11) UNSIGNED NOT NULL DEFAULT '0',
-	`hidden` tinyint(1) UNSIGNED NOT NULL DEFAULT '0',
-	`disabled` tinyint(1) UNSIGNED NOT NULL DEFAULT '0',
-	`deleted` tinyint(1) UNSIGNED NOT NULL DEFAULT '0',
-	`createdTime` int(11) UNSIGNED NOT NULL DEFAULT '0',
-	`createdUserUid` int(11) UNSIGNED NOT NULL DEFAULT '0',
-	`lastModifiedTime` int(11) UNSIGNED NOT NULL DEFAULT '0',
-	`lastModifiedUserUid` int(11) UNSIGNED NOT NULL DEFAULT '0',
-	`validTimeStart` int(11) UNSIGNED NOT NULL DEFAULT '0',
-	`validTimeEnd` int(11) UNSIGNED NOT NULL DEFAULT '0',
-
-	`firstname` varchar(50) NOT NULL DEFAULT '',
-	`lastname` varchar(50) NOT NULL DEFAULT '',
-	`sex` boolean ,
-	`age` int(2) UNSIGNED NOT NULL DEFAULT '0',
-	`job` varchar(50) NOT NULL DEFAULT '',
 
 	PRIMARY KEY (`uid`)
 )ENGINE=InnoDB DEFAULT CHARSET=utf8;
