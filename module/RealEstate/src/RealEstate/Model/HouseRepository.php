4ad00e6d4519e2769857fc339d34bcbf3df75e4a
fanboya <chanpiset.som@gmail.com>
2012-12-21 14:57:50 +0700

    create pageination in HousesList

--- module/RealEstate/src/RealEstate/Model/HouseRepository.php
+++ module/RealEstate/src/RealEstate/Model/HouseRepository.php
@@ -21,13 +21,22 @@ class HouseRepository {
         
         public function listAll() {
 		$adapter = $this->tableGateway->getAdapter();
-                $sql = 'select h.uid,h.isRoomRent,h.lastModifiedTime as lastUpdate,t.title as Type, h.cost as houseCost, max(r.cost) as roomCostMax, min(r.cost) as roomCostMin, a.*
+                $sql = 'select h.uid,h.isRoomRent,h.lastModifiedTime as lastUpdate,t.title as Type,COUNT(r.uid) as NumberofRooms, h.cost as houseCost, max(r.cost) as roomCostMax, min(r.cost) as roomCostMin, a.*
                         from houses as h
                         left join rooms as r on h.uid = r.houseUid
                         join address as a on h.addressUid = a.uid
                         join house_types as t on h.typeUid = t.uid
                         group by r.houseUid
                         order by h.lastModifiedTime';
+		$statement = $adapter->query($sql);
+		$resultSet = $statement->execute();
+		
+		return $resultSet;
+	}
+	
+	public function countAll() {
+		$adapter = $this->tableGateway->getAdapter();
+                $sql = 'select count(uid) as count from houses';
 		$statement = $adapter->query($sql);
 		$resultSet = $statement->execute();
 		
