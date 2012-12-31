b030680914798d5735953a10f870701344f2c7a0
Liratanak <annliratanak@gmail.com>
2012-12-31 16:55:10 +0700

    new Group entity

--- module/RealEstate/src/RealEstate/Entity/Group.php
+++ module/RealEstate/src/RealEstate/Entity/Group.php
@@ -10,417 +10,404 @@ use Doctrine\ORM\Mapping as ORM;
  * @ORM\Table(name="group")
  * @ORM\Entity(repositoryClass="RealEstate\Repository\GroupRepository")
  */
-class Group
-{
-    /**
-     * @var integer
-     *
-     * @ORM\Column(name="id", type="integer", nullable=false)
-     * @ORM\Id
-     * @ORM\GeneratedValue(strategy="IDENTITY")
-     */
-    private $id;
+class Group {
 
-    /**
-     * @var integer
-     *
-     * @ORM\Column(name="pid", type="integer", nullable=false)
-     */
-    private $pid;
+	/**
+	 * @var integer
+	 *
+	 * @ORM\Column(name="id", type="integer", nullable=false)
+	 * @ORM\Id
+	 * @ORM\GeneratedValue(strategy="IDENTITY")
+	 */
+	private $id;
 
-    /**
-     * @var boolean
-     *
-     * @ORM\Column(name="hidden", type="boolean", nullable=false)
-     */
-    private $hidden;
+	/**
+	 * @var integer
+	 *
+	 * @ORM\Column(name="pid", type="integer", nullable=false)
+	 */
+	private $pid;
 
-    /**
-     * @var boolean
-     *
-     * @ORM\Column(name="disabled", type="boolean", nullable=false)
-     */
-    private $disabled;
+	/**
+	 * @var boolean
+	 *
+	 * @ORM\Column(name="hidden", type="boolean", nullable=false)
+	 */
+	private $hidden;
 
-    /**
-     * @var boolean
-     *
-     * @ORM\Column(name="deleted", type="boolean", nullable=false)
-     */
-    private $deleted;
+	/**
+	 * @var boolean
+	 *
+	 * @ORM\Column(name="disabled", type="boolean", nullable=false)
+	 */
+	private $disabled;
 
-    /**
-     * @var integer
-     *
-     * @ORM\Column(name="created_time", type="integer", nullable=false)
-     */
-    private $createdTime;
+	/**
+	 * @var boolean
+	 *
+	 * @ORM\Column(name="deleted", type="boolean", nullable=false)
+	 */
+	private $deleted;
 
-    /**
-     * @var integer
-     *
-     * @ORM\Column(name="last_modified_time", type="integer", nullable=false)
-     */
-    private $lastModifiedTime;
+	/**
+	 * @var integer
+	 *
+	 * @ORM\Column(name="created_time", type="integer", nullable=false)
+	 */
+	private $createdTime;
 
-    /**
-     * @var integer
-     *
-     * @ORM\Column(name="valid_time_start", type="integer", nullable=false)
-     */
-    private $validTimeStart;
+	/**
+	 * @var integer
+	 *
+	 * @ORM\Column(name="last_modified_time", type="integer", nullable=false)
+	 */
+	private $lastModifiedTime;
 
-    /**
-     * @var integer
-     *
-     * @ORM\Column(name="valid_time_end", type="integer", nullable=false)
-     */
-    private $validTimeEnd;
+	/**
+	 * @var integer
+	 *
+	 * @ORM\Column(name="valid_time_start", type="integer", nullable=false)
+	 */
+	private $validTimeStart;
 
-    /**
-     * @var string
-     *
-     * @ORM\Column(name="title", type="string", length=64, nullable=false)
-     */
-    private $title;
+	/**
+	 * @var integer
+	 *
+	 * @ORM\Column(name="valid_time_end", type="integer", nullable=false)
+	 */
+	private $validTimeEnd;
 
-    /**
-     * @var \Doctrine\Common\Collections\Collection
-     *
-     * @ORM\ManyToMany(targetEntity="RealEstate\Entity\Permission", inversedBy="group")
-     * @ORM\JoinTable(name="group_permission",
-     *   joinColumns={
-     *     @ORM\JoinColumn(name="group", referencedColumnName="id")
-     *   },
-     *   inverseJoinColumns={
-     *     @ORM\JoinColumn(name="permission", referencedColumnName="id")
-     *   }
-     * )
-     */
-    private $permission;
+	/**
+	 * @var string
+	 *
+	 * @ORM\Column(name="title", type="string", length=64, nullable=false)
+	 */
+	private $title;
 
-    /**
-     * @var \RealEstate\Entity\User
-     *
-     * @ORM\ManyToOne(targetEntity="RealEstate\Entity\User")
-     * @ORM\JoinColumns({
-     *   @ORM\JoinColumn(name="created_user", referencedColumnName="id")
-     * })
-     */
-    private $createdUser;
+	/**
+	 * @var \Doctrine\Common\Collections\Collection
+	 *
+	 * @ORM\ManyToMany(targetEntity="RealEstate\Entity\Permission", inversedBy="group")
+	 * @ORM\JoinTable(name="group_permission",
+	 *   joinColumns={
+	 *     @ORM\JoinColumn(name="group", referencedColumnName="id")
+	 *   },
+	 *   inverseJoinColumns={
+	 *     @ORM\JoinColumn(name="permission", referencedColumnName="id")
+	 *   }
+	 * )
+	 */
+	private $permission;
 
-    /**
-     * @var \RealEstate\Entity\User
-     *
-     * @ORM\ManyToOne(targetEntity="RealEstate\Entity\User")
-     * @ORM\JoinColumns({
-     *   @ORM\JoinColumn(name="last_modified_user", referencedColumnName="id")
-     * })
-     */
-    private $lastModifiedUser;
+	/**
+	 * @var \RealEstate\Entity\User
+	 *
+	 * @ORM\ManyToOne(targetEntity="RealEstate\Entity\User")
+	 * @ORM\JoinColumns({
+	 *   @ORM\JoinColumn(name="created_user", referencedColumnName="id")
+	 * })
+	 */
+	private $createdUser;
 
-    /**
-     * Constructor
-     */
-    public function __construct()
-    {
-        $this->permission = new \Doctrine\Common\Collections\ArrayCollection();
-    }
-    
+	/**
+	 * @var \RealEstate\Entity\User
+	 *
+	 * @ORM\ManyToOne(targetEntity="RealEstate\Entity\User")
+	 * @ORM\JoinColumns({
+	 *   @ORM\JoinColumn(name="last_modified_user", referencedColumnName="id")
+	 * })
+	 */
+	private $lastModifiedUser;
 
-    /**
-     * Get id
-     *
-     * @return integer 
-     */
-    public function getId()
-    {
-        return $this->id;
-    }
+	/**
+	 * Constructor
+	 */
+	public function __construct() {
+		$this->pid = 0;
+		$this->deleted = 0;
+		$this->disabled = 0;
+		$this->hidden = 0;
+		
+		$this->createdTime = time();
+		$this->lastModifiedTime = time();
+		$this->validTimeEnd = time();
+		$this->validTimeStart = time();
+		
+		$this->permission = new \Doctrine\Common\Collections\ArrayCollection();
+	}
 
-    /**
-     * Set pid
-     *
-     * @param integer $pid
-     * @return Group
-     */
-    public function setPid($pid)
-    {
-        $this->pid = $pid;
-    
-        return $this;
-    }
+	/**
+	 * Get id
+	 *
+	 * @return integer 
+	 */
+	public function getId() {
+		return $this->id;
+	}
 
-    /**
-     * Get pid
-     *
-     * @return integer 
-     */
-    public function getPid()
-    {
-        return $this->pid;
-    }
+	/**
+	 * Set pid
+	 *
+	 * @param integer $pid
+	 * @return Group
+	 */
+	public function setPid($pid) {
+		$this->pid = $pid;
 
-    /**
-     * Set hidden
-     *
-     * @param boolean $hidden
-     * @return Group
-     */
-    public function setHidden($hidden)
-    {
-        $this->hidden = $hidden;
-    
-        return $this;
-    }
+		return $this;
+	}
 
-    /**
-     * Get hidden
-     *
-     * @return boolean 
-     */
-    public function getHidden()
-    {
-        return $this->hidden;
-    }
+	/**
+	 * Get pid
+	 *
+	 * @return integer 
+	 */
+	public function getPid() {
+		return $this->pid;
+	}
 
-    /**
-     * Set disabled
-     *
-     * @param boolean $disabled
-     * @return Group
-     */
-    public function setDisabled($disabled)
-    {
-        $this->disabled = $disabled;
-    
-        return $this;
-    }
+	/**
+	 * Set hidden
+	 *
+	 * @param boolean $hidden
+	 * @return Group
+	 */
+	public function setHidden($hidden) {
+		$this->hidden = $hidden;
 
-    /**
-     * Get disabled
-     *
-     * @return boolean 
-     */
-    public function getDisabled()
-    {
-        return $this->disabled;
-    }
+		return $this;
+	}
 
-    /**
-     * Set deleted
-     *
-     * @param boolean $deleted
-     * @return Group
-     */
-    public function setDeleted($deleted)
-    {
-        $this->deleted = $deleted;
-    
-        return $this;
-    }
+	/**
+	 * Get hidden
+	 *
+	 * @return boolean 
+	 */
+	public function getHidden() {
+		return $this->hidden;
+	}
 
-    /**
-     * Get deleted
-     *
-     * @return boolean 
-     */
-    public function getDeleted()
-    {
-        return $this->deleted;
-    }
+	/**
+	 * Set disabled
+	 *
+	 * @param boolean $disabled
+	 * @return Group
+	 */
+	public function setDisabled($disabled) {
+		$this->disabled = $disabled;
 
-    /**
-     * Set createdTime
-     *
-     * @param integer $createdTime
-     * @return Group
-     */
-    public function setCreatedTime($createdTime)
-    {
-        $this->createdTime = $createdTime;
-    
-        return $this;
-    }
+		return $this;
+	}
 
-    /**
-     * Get createdTime
-     *
-     * @return integer 
-     */
-    public function getCreatedTime()
-    {
-        return $this->createdTime;
-    }
+	/**
+	 * Get disabled
+	 *
+	 * @return boolean 
+	 */
+	public function getDisabled() {
+		return $this->disabled;
+	}
 
-    /**
-     * Set lastModifiedTime
-     *
-     * @param integer $lastModifiedTime
-     * @return Group
-     */
-    public function setLastModifiedTime($lastModifiedTime)
-    {
-        $this->lastModifiedTime = $lastModifiedTime;
-    
-        return $this;
-    }
+	/**
+	 * Set deleted
+	 *
+	 * @param boolean $deleted
+	 * @return Group
+	 */
+	public function setDeleted($deleted) {
+		$this->deleted = $deleted;
 
-    /**
-     * Get lastModifiedTime
-     *
-     * @return integer 
-     */
-    public function getLastModifiedTime()
-    {
-        return $this->lastModifiedTime;
-    }
+		return $this;
+	}
 
-    /**
-     * Set validTimeStart
-     *
-     * @param integer $validTimeStart
-     * @return Group
-     */
-    public function setValidTimeStart($validTimeStart)
-    {
-        $this->validTimeStart = $validTimeStart;
-    
-        return $this;
-    }
+	/**
+	 * Get deleted
+	 *
+	 * @return boolean 
+	 */
+	public function getDeleted() {
+		return $this->deleted;
+	}
 
-    /**
-     * Get validTimeStart
-     *
-     * @return integer 
-     */
-    public function getValidTimeStart()
-    {
-        return $this->validTimeStart;
-    }
+	/**
+	 * Set createdTime
+	 *
+	 * @param integer $createdTime
+	 * @return Group
+	 */
+	public function setCreatedTime($createdTime) {
+		$this->createdTime = $createdTime;
 
-    /**
-     * Set validTimeEnd
-     *
-     * @param integer $validTimeEnd
-     * @return Group
-     */
-    public function setValidTimeEnd($validTimeEnd)
-    {
-        $this->validTimeEnd = $validTimeEnd;
-    
-        return $this;
-    }
+		return $this;
+	}
 
-    /**
-     * Get validTimeEnd
-     *
-     * @return integer 
-     */
-    public function getValidTimeEnd()
-    {
-        return $this->validTimeEnd;
-    }
+	/**
+	 * Get createdTime
+	 *
+	 * @return integer 
+	 */
+	public function getCreatedTime() {
+		return $this->createdTime;
+	}
 
-    /**
-     * Set title
-     *
-     * @param string $title
-     * @return Group
-     */
-    public function setTitle($title)
-    {
-        $this->title = $title;
-    
-        return $this;
-    }
+	/**
+	 * Set lastModifiedTime
+	 *
+	 * @param integer $lastModifiedTime
+	 * @return Group
+	 */
+	public function setLastModifiedTime($lastModifiedTime) {
+		$this->lastModifiedTime = $lastModifiedTime;
 
-    /**
-     * Get title
-     *
-     * @return string 
-     */
-    public function getTitle()
-    {
-        return $this->title;
-    }
+		return $this;
+	}
 
-    /**
-     * Add permission
-     *
-     * @param \RealEstate\Entity\Permission $permission
-     * @return Group
-     */
-    public function addPermission(\RealEstate\Entity\Permission $permission)
-    {
-        $this->permission[] = $permission;
-    
-        return $this;
-    }
+	/**
+	 * Get lastModifiedTime
+	 *
+	 * @return integer 
+	 */
+	public function getLastModifiedTime() {
+		return $this->lastModifiedTime;
+	}
 
-    /**
-     * Remove permission
-     *
-     * @param \RealEstate\Entity\Permission $permission
-     */
-    public function removePermission(\RealEstate\Entity\Permission $permission)
-    {
-        $this->permission->removeElement($permission);
-    }
+	/**
+	 * Set validTimeStart
+	 *
+	 * @param integer $validTimeStart
+	 * @return Group
+	 */
+	public function setValidTimeStart($validTimeStart) {
+		$this->validTimeStart = $validTimeStart;
 
-    /**
-     * Get permission
-     *
-     * @return \Doctrine\Common\Collections\Collection 
-     */
-    public function getPermission()
-    {
-        return $this->permission;
-    }
+		return $this;
+	}
 
-    /**
-     * Set createdUser
-     *
-     * @param \RealEstate\Entity\User $createdUser
-     * @return Group
-     */
-    public function setCreatedUser(\RealEstate\Entity\User $createdUser = null)
-    {
-        $this->createdUser = $createdUser;
-    
-        return $this;
-    }
+	/**
+	 * Get validTimeStart
+	 *
+	 * @return integer 
+	 */
+	public function getValidTimeStart() {
+		return $this->validTimeStart;
+	}
 
-    /**
-     * Get createdUser
-     *
-     * @return \RealEstate\Entity\User 
-     */
-    public function getCreatedUser()
-    {
-        return $this->createdUser;
-    }
+	/**
+	 * Set validTimeEnd
+	 *
+	 * @param integer $validTimeEnd
+	 * @return Group
+	 */
+	public function setValidTimeEnd($validTimeEnd) {
+		$this->validTimeEnd = $validTimeEnd;
 
-    /**
-     * Set lastModifiedUser
-     *
-     * @param \RealEstate\Entity\User $lastModifiedUser
-     * @return Group
-     */
-    public function setLastModifiedUser(\RealEstate\Entity\User $lastModifiedUser = null)
-    {
-        $this->lastModifiedUser = $lastModifiedUser;
-    
-        return $this;
-    }
+		return $this;
+	}
 
-    /**
-     * Get lastModifiedUser
-     *
-     * @return \RealEstate\Entity\User 
-     */
-    public function getLastModifiedUser()
-    {
-        return $this->lastModifiedUser;
-    }
+	/**
+	 * Get validTimeEnd
+	 *
+	 * @return integer 
+	 */
+	public function getValidTimeEnd() {
+		return $this->validTimeEnd;
+	}
+
+	/**
+	 * Set title
+	 *
+	 * @param string $title
+	 * @return Group
+	 */
+	public function setTitle($title) {
+		$this->title = $title;
+
+		return $this;
+	}
+
+	/**
+	 * Get title
+	 *
+	 * @return string 
+	 */
+	public function getTitle() {
+		return $this->title;
+	}
+
+	/**
+	 * Add permission
+	 *
+	 * @param \RealEstate\Entity\Permission $permission
+	 * @return Group
+	 */
+	public function addPermission(\RealEstate\Entity\Permission $permission) {
+		$this->permission[] = $permission;
+
+		return $this;
+	}
+
+	/**
+	 * Remove permission
+	 *
+	 * @param \RealEstate\Entity\Permission $permission
+	 */
+	public function removePermission(\RealEstate\Entity\Permission $permission) {
+		$this->permission->removeElement($permission);
+	}
+
+	/**
+	 * Get permission
+	 *
+	 * @return \Doctrine\Common\Collections\Collection 
+	 */
+	public function getPermission() {
+		return $this->permission;
+	}
+
+	/**
+	 * Set createdUser
+	 *
+	 * @param \RealEstate\Entity\User $createdUser
+	 * @return Group
+	 */
+	public function setCreatedUser(\RealEstate\Entity\User $createdUser = null) {
+		$this->createdUser = $createdUser;
+
+		return $this;
+	}
+
+	/**
+	 * Get createdUser
+	 *
+	 * @return \RealEstate\Entity\User 
+	 */
+	public function getCreatedUser() {
+		return $this->createdUser;
+	}
+
+	/**
+	 * Set lastModifiedUser
+	 *
+	 * @param \RealEstate\Entity\User $lastModifiedUser
+	 * @return Group
+	 */
+	public function setLastModifiedUser(\RealEstate\Entity\User $lastModifiedUser = null) {
+		$this->lastModifiedUser = $lastModifiedUser;
+
+		return $this;
+	}
+
+	/**
+	 * Get lastModifiedUser
+	 *
+	 * @return \RealEstate\Entity\User 
+	 */
+	public function getLastModifiedUser() {
+		return $this->lastModifiedUser;
+	}
+	
+	public function onPrePersist() {
+		
+	}
+
 }
