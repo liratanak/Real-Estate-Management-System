36db53129f8c180d7503b794d2c4ce315f91bd3b
Liratanak <annliratanak@gmail.com>
2013-01-29 11:01:55 +0700

    rate for house

--- module/RealEstate/src/RealEstate/Controller/RateController.php
+++ module/RealEstate/src/RealEstate/Controller/RateController.php
@@ -8,8 +8,12 @@ use Zend\View\Model\ViewModel;
 class RateController extends AbstractActionController {
 
 	public function rateAction() {
-		
 		if ($this->getRequest()->isXmlHttpRequest()) {
+			$postData = $this->getRequest()->getPost();
+			$user = $this->getServiceLocator()->get('zfcuser_user_service')->getAuthService()->getIdentity();
+			
+			$house = $this->getEntityManager()->find('RealEstate\Entity\House' , $postData->houseId);
+			
 			
 			$this->layout('layout/ajax');
 		}
